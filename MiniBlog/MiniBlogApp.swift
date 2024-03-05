fb85930577935f1cb50f5bbb8065261658a19814
danieiOS <daneilosong98@gmail.com>
2024-03-05 20:01:51 +0900

    Initial Commit

---
+++ MiniBlog/MiniBlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniBlogApp.swift
+//  MiniBlog
+//
+//  Created by 송성욱 on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MiniBlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
